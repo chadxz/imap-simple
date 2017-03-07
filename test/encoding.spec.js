f90e19579d14cbb2cb8ff3dfaf34fb6c57ac3d2a
antti-mann <antti.mann@gmail.com>
2017-03-07 09:51:04 +0200

    Decode messages with iconvlite

--- test/encoding.spec.js
+++ test/encoding.spec.js
@@ -6,7 +6,7 @@ describe("imap-simple", function () {
     var getEncoding = require("../lib/helpers/encoding");
 
     it("correct encoding is found ", function () {
-        expect(getEncoding({ 'content-type': [ 'text/plain; charset="iso-8859-1"' ] })).to.equal('latin1');
+        expect(getEncoding({ 'content-type': [ 'text/plain; charset="iso-8859-1"' ] })).to.equal('iso-8859-1');
         expect(getEncoding({})).to.equal('utf8');
     });
 });
